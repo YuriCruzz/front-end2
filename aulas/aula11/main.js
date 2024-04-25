58d80bd140fc3c7684b1705b1c23e2bd49d95e37
Hamurabi Araújo <hamurabi.araujo@al.rn.leg.br>
2024-04-24 21:14:32 -0300

    aula 11 - método de registrar usuário adicionado

--- aulas/aula11/main.js
+++ aulas/aula11/main.js
@@ -1,10 +1,9 @@
 function fazerLogin() {
-  let emailInserido = document.getElementById('email').value;
-  let senhaInserida = document.getElementById('password').value;
+  let { email, password } = _recuperarCampos();
 
   const credenciais = {
-    email: emailInserido,
-    password: senhaInserida
+    email: email.value,
+    password: password.value
   };
 
   fetch('https://reqres.in/api/login', {
@@ -25,4 +24,40 @@ function fazerLogin() {
       boasVindas.style = "background-color: red; color: white";
     }
   }); 
+}
+
+function registrar() {
+   let { email, password } = _recuperarCampos();
+
+  const credenciais = {
+    email: email.value,
+    password: password.value
+  };
+
+  fetch('https://reqres.in/api/register', {
+    method: 'POST',
+    body: JSON.stringify(credenciais),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  }).then((response) => response.json())
+  .then((json) => {
+    let boasVindas = document.getElementById('boas-vindas');
+
+    if(json.token) {
+      boasVindas.innerText = 'Cadastro criado com sucesso';
+      boasVindas.style = "background-color: green; color: white";
+    } else {
+      boasVindas.innerText = 'Erro ao realizar cadastro';
+      boasVindas.style = "background-color: red; color: white";
+    }
+  }); 
+ 
+}
+
+function _recuperarCampos() {
+  return {
+    email: document.getElementById('email'),
+    password: document.getElementById('password'),
+  };
 }
